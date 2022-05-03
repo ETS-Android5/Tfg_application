51483f6dae6da6126844e7754ad4d29563c6487c
alvaromch <77532797+alvaromch@users.noreply.github.com>
2022-05-03 12:32:33 +0200

    Update Sign_in.java

--- app/src/main/java/com/example/tfg_plication/Sign_in.java
+++ app/src/main/java/com/example/tfg_plication/Sign_in.java
@@ -22,6 +22,7 @@ public class Sign_in extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+        getSupportActionBar().hide();
 
 
 
