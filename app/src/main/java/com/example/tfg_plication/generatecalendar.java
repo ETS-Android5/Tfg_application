cec077597a4529feea7ecb96e4298081e90f6533
alvaromch <77532797+alvaromch@users.noreply.github.com>
2022-05-09 17:28:09 +0200

    m

--- app/src/main/java/com/example/tfg_plication/generatecalendar.java
+++ app/src/main/java/com/example/tfg_plication/generatecalendar.java
@@ -4,9 +4,9 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class Generatecalendar extends AppCompatActivity {
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_generatecalendar);
-    }
-}
+//public class Generatecalendar extends AppCompatActivity {
+  //  protected void onCreate(Bundle savedInstanceState) {
+    //    super.onCreate(savedInstanceState);
+      //  setContentView(R.layout.activity_generatecalendar);
+    //}
+//}
