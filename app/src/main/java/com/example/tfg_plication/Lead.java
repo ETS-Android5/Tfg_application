58f287dc537e0d670775a3a97ee57ee517241318
alvaromch <77532797+alvaromch@users.noreply.github.com>
2022-05-14 12:06:22 +0200

    lead

---
+++ app/src/main/java/com/example/tfg_plication/Lead.java
@@ -0,0 +1,14 @@
+package com.example.tfg_plication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Lead extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lead);
+    }
+}
